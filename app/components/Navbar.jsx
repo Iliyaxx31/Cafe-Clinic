b074de833236ebb2cff0fc5c903c1094869cfa21
Iliyaxx31 <iliyamakin17@gmail.com>
2026-08-18 18:57:02 +0330

    change espresso

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { CiCoffeeCup, CiGlass } from "react-icons/ci";
 import { RiDrinks2Line } from "react-icons/ri";
 import { TbMilkshake } from "react-icons/tb";
 import { SiGitea } from "react-icons/si";
-import { FaHamburger, FaBreadSlice, FaPizzaSlice } from "react-icons/fa";
+import { FaHamburger, FaCookie , FaBreadSlice, FaPizzaSlice } from "react-icons/fa";
 import Kategori from "./Kategori";
 import { SiBuymeacoffee } from "react-icons/si";
 import { BiSolidBowlHot } from "react-icons/bi";
@@ -28,6 +28,7 @@ const specialIcons = {
   برگر: FaHamburger, // Burger
  "نوشیدنی گرم":  BiSolidBowlHot ,
   "ملک شیک": TbMilkshake, // Milkshake (farklı)
+  "کیک و کوکی":FaCookie
 };
 
 const Navbar = ({ onSelectCategory, activeIndex, categories }) => {
